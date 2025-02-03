4318b74d1a45fd14eb5a3b1e54f6ff18d0a7b376
fano <fanorena@gmail.com>
2025-02-03 07:44:29 -0500

    primer post

---
+++ operadores.py
@@ -0,0 +1,3 @@
+var = 1
+mar = 2
+print("su suma es ",var+mar)
---
+++ tiposdatos.py
@@ -0,0 +1,46 @@
+""""
+print("Hola amigos paisas")
+edad = 52
+print("edad")
+peso = 78
+print(peso)
+nombre = "Carlos"
+print(nombre)
+esMadreSoltera = False
+num1 =5
+num2 = 6
+suma = num1 + num2
+print(suma)
+
+nombre = "Carlos"
+apellido = "Fadul"
+print("Hola "+nombre+ " "+apellido)
+
+numero1 = "5"
+numero2 = "6"
+suma = numero1 + numero2
+print(suma)
+
+
+nombre = input("Ingrese su nombre: ")
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+suma = num1 + num2
+resta = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+print("Hola "+nombre)
+print("La suma de los dos numeros es: ",suma)
+print("La resta de los dos numeros es: ",resta)
+print("La multiplicacion de los dos numeros es: ",mult)
+print("La division de los dos numeros es: ",div)
+nombrecliente = input("Nombre del Cliente ")
+preciop1 = int(input("Ingrese el precio del producto 1: "))
+preciop2 = int(input("Ingrese el precio del producto 2: "))
+preciop3 = int(input("Ingrese el precio del producto 3: "))
+costototal = preciop1 + preciop2 + preciop3
+print("El costo total es de ", costototal)
+valorpagado = int(input("Ingrese el valor pagado por el cliente "))
+devuelta = valorpagado - costototal
+print("Debe devolver a ",nombrecliente, " ",devuelta)
+"""
